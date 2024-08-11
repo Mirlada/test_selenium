3c9f090f6cb0f11ca6825e6f3bab10ce1ca1732d
Mirlada <114110265+Mirlada@users.noreply.github.com>
2024-08-11 14:17:31 +0300

    Add files via upload

--- test_selenium.py
+++ test_selenium.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
+from pyvirtualdisplay import Display  # Установите pyvirtualdisplay через pip
+
+# Запускаем виртуальный дисплей
+display = Display(visible=0, size=(1024, 768))
+display.start()
 
 # Укажите путь к вашему geckodriver, если он не в PATH
 service = Service('/usr/local/bin/geckodriver')
@@ -21,3 +26,6 @@ driver.implicitly_wait(10)
 
 # Закроем браузер
 driver.quit()
+
+# Остановим виртуальный дисплей
+display.stop()
